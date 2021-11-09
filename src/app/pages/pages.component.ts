43ddf55bb70b5e5e89e682d3af51426a9ee6cf06
Ashwath Bharadwaj <ashwath.bharadwaj@nineleaps.com>
2021-11-10 04:11:59 +0530

    Fix

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,7 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
+import { ChooseAppSettingsModalComponent } from '../core/auth/components/choose-app-settings-modal/choose-app-settings-modal.component';
+import { packsDataSampleDoctor } from '../mock-data/constants';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -29,12 +32,34 @@ export class PagesComponent implements OnInit {
 
   currentNavlink:any = this.navlinks[0];
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() { }
 
   gotoNavlink(link:any){
     this.currentNavlink = link;
+  }
+
+  openSettingsDialog(): void {
+    let dialogRef = this.dialog.open(ChooseAppSettingsModalComponent, {
+      hasBackdrop: true,
+      disableClose: false,
+      height: '100vh',
+      minWidth: '90%',
+      position: {
+        right: '0px',
+        bottom: '0px',
+      },
+      data: {
+        pack: packsDataSampleDoctor[0]
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
